a5608c8aaea2ecd3de1a82121aced8eb99f900c1
jonko0493 <jonko0493@protonmail.com>
2021-12-12 14:55:24 -0800

    Clean up intermediate directory

--- build_arc.bat
+++ build_arc.bat
@@ -1,3 +1,4 @@
-rmdir /s /q "intermediate"
 mkdir "intermediate\files"
+copy intermediate\files\* patch\Heiretsu\files
 %HEIRETSU_CLI_PATH% --generate-patch -o "intermediate"
+rmdir /s /q "intermediate"
--- build_arc.ps1
+++ build_arc.ps1
@@ -2,9 +2,8 @@ Param(
   [string]$heiretsuCli = "tools/HaruhiHeiretsuCLI/HaruhiHeiretsuCLI.exe"
 )
 
-Remove-Item -Force -Recurse "./intermediate"
 mkdir -p "./intermediate/files"
 # Run HaruhiHeiretsuCLI to patch the archives and output them to the intermediate directory
-# In the meantime, copy files there directly after editing them with the editor
 Copy-Item -Path "./intermediate/files" -Destination "./patch/Heirestu/files"
 & $heiretsuCli --generate-patch -o "intermediate"
+Remove-Item -Force -Recurse "./intermediate"
