ad046820059487f73d25b5270b51f2d6d6f8243c
jonko0493 <jonko0493@protonmail.com>
2021-12-12 23:58:35 -0800

    Fix build.ps1

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ Param(
 mkdir -p "./intermediate/files"
 # Run HaruhiHeiretsuCLI to patch the archives and output them to the intermediate directory
 # In the meantime, copy files there directly after editing them with the editor
-Copy-Item -Path "./intermediate/files" -Destination "./patch/Heiretsu/files" -Force
+Copy-Item -Path "./intermediate/files" -Destination "./patch/Heiretsu/" -Force
 & $heiretsuCli --generate-patch -o "intermediate"
 & $wiinject -f "src" -i "801B3520" -e "801B5184" -p "./intermediate/Heiretsu_base.xml" -o "patch" -n "Heiretsu"
 Remove-Item -Force -Recurse "./intermediate"
