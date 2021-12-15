fc5c4a6f379af01f36879210212f5743ff067dde
jonko0493 <jonko0493@protonmail.com>
2021-12-14 21:46:08 -0800

    Add first grp to archives, update build script for newest CLI ver, add file maps

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
 Param(
   [string]$heiretsuCli = "tools/HaruhiHeiretsuCLI/HaruhiHeiretsuCLI.exe",
   [string]$wiinject = "tools/Wiinject/Wiinject.exe",
+  [string]$langCode = "en",
   [switch]$patchDol
 )
 
-mkdir -p "./intermediate/files"
-# Run HaruhiHeiretsuCLI to patch the archives and output them to the intermediate directory
-# In the meantime, copy files there directly after editing them with the editor
-Copy-Item -Path "./intermediate/files" -Destination "./patch/Heiretsu/" -Force
+mkdir -p "./patch/Heiretsu/files"
+mkdir -p "./intermediate"
+& $heiretsuCli --replace-graphics -m "./extracted/DATA/files/mcb0.bln" -b "./extracted/DATA/files/grp.bin" -r "./archives/grp/$langCode/" -o "./patch/Heiretsu/files"
 & $heiretsuCli --generate-patch -o "intermediate"
 & $wiinject -f "src" -i "801B3520" -e "801B5184" -p "./intermediate/Heiretsu_base.xml" -o "patch" -n "Heiretsu"
 Remove-Item -Force -Recurse "./intermediate"
