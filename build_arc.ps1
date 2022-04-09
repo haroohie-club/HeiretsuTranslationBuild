1c92e0de01ba101580b732160c5ca2c6bfbc9cbd
jonko0493 <jonko0493@protonmail.com>
2022-04-08 22:20:27 -0700

    Switch up file replacement

--- build_arc.ps1
+++ build_arc.ps1
@@ -6,4 +6,4 @@ Param(
 )
 
 & $heiretsuCli replace-font -m "./extracted/DATA/files/mcb0.bln" -g "./extracted/DATA/files/grp.bin" -f "$assetsFolder/font/font.ttf" -s $fontSize -b "!" -e "z" -o "./patch/Heiretsu/files"
-& $heiretsuCli replace-graphics -m "./patch/Heiretsu/files/mcb0.bln" -g "./patch/Heiretsu/files/grp.bin" -r "$assetsFolder/graphics/$langCode/" -o "./patch/Heiretsu/files"
+& $heiretsuCli replace-files -m "./extracted/DATA/files/mcb0.bln" -d "./extracted/DATA/files/dat.bin" -e "./extracted/DATA/files/evt.bin" -g "./extracted/DATA/files/grp.bin" -s "./extracted/DATA/files/scr.bin" -r "$assetsFolder/$langCode" -o "./patch/Heiretsu/files"
