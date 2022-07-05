6a29ff15d131b686a0f3b03a457de7a46e9d48b2
jonko0493 <jonko0493@protonmail.com>
2022-07-05 06:58:50 -0700

    Update for modern font hack

--- build_arc.ps1
+++ build_arc.ps1
@@ -9,8 +9,9 @@ Param(
 if (-not (Test-Path -Path ./patch/Heiretsu/files)) {
     New-Item -Path ./patch/Heiretsu/files -ItemType Directory
 }
-# & $heiretsuCli replace-font -m "./extracted/DATA/files/mcb0.bln" -g "./extracted/DATA/files/grp.bin" -f "$assetsFolder/font/font.ttf" -s $fontSize -b "!" -e "z" -o "./patch/Heiretsu/files"
-& $heiretsuCli replace-files -m "./extracted/DATA/files/mcb0.bln" -d "./extracted/DATA/files/dat.bin" -e "./extracted/DATA/files/evt.bin" -g "./extracted/DATA/files/grp.bin" -s "./extracted/DATA/files/scr.bin" -r "$assetsFolder/$langCode" -o "./patch/Heiretsu/files"
+& $heiretsuCli generate-font-replacement -f "$assetsFolder/font/font.ttf" -s $fontSize -j "./font_replacement_map.json" -c "./src/HeiretsuTranslation/font_hack.c" -e "€‚ƒ`„…†‡ˆ‰Š‹ŒŽ‘’`“`”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÉËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ"
+& $heiretsuCli replace-font -m "./extracted/DATA/files/mcb0.bln" -g "./extracted/DATA/files/grp.bin" -f "$assetsFolder/font/font.ttf" -s $fontSize -r "./font_replacement_map.json" -o "./patch/Heiretsu/files"
+& $heiretsuCli replace-files -m "./patch/Heiretsu/files/mcb0.bln" -d "./extracted/DATA/files/dat.bin" -e "./extracted/DATA/files/evt.bin" -g "./patch/Heiretsu/files/grp.bin" -s "./extracted/DATA/files/scr.bin" -r "$assetsFolder/$langCode" -o "./patch/Heiretsu/files"
 if ($copyTo) {
     Write-Host "Copying output to '$copyTo'..."
     Copy-Item -Path "./patch/Heiretsu" -Destination $copyTo -Recurse -Force
