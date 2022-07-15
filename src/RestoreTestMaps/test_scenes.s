ce938ede02f4ca9bbef272a86806a15658e4f625
jonko0493 <jonko0493@protonmail.com>
2022-07-15 06:13:34 -0700

    Restore original clubroom too (also makes things simpler lol)

--- src/RestoreTestMaps/test_scenes.s
+++ src/RestoreTestMaps/test_scenes.s
@@ -1,29 +1,12 @@
-repl_8001AA28:
-    mflr 0
-
-hook_8001AA2C:
-    cmpwi 3,0x02
-    ble special
-    cmpwi 3,0x0C
-    bne testscene
-    
-    clubroom:
-    cmpwi 3,0x0B
-    blr
-
-    special:
-    cmpwi 3,-1
-    blr
-
-    testscene:
-    cmpwi 3,0x0D
-    blr
-
 repl_8001AA30:
-    mtlr 0
+    cmpwi 3,0x0D
 
 hook_8002B338:
     stw 0,0x54(1)
+    lwz 0,-0x7680(13)
+    lwz 4,-0x767C(13)
+    stw 4,0x58(1)
+    stw 0,0x5C(1)
     lwz 0,-0x7688(13)
     lwz 4,-0x7684(13)
     stw 4,0x60(1)
